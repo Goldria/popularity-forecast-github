0eb22f85b87f1519918da1dae4ed52a5987a80d0
Goldria <dasha@igold.pp.ru>
2023-10-24 17:10:14 +0500

    Page parsing is moved to the method.

--- src/main.py
+++ src/main.py
@@ -6,49 +6,58 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-url = "https://play.clickhouse.com/play?user=play"
 
-offset = 0
-data = []
+def parsingHTML(limit):
+    url = "https://play.clickhouse.com/play?user=play"
 
-while offset < 10_000:
+    offset = 0
+    data = []
     driver = webdriver.Chrome()
-    driver.get(url)
 
-    query_input = driver.find_element(By.ID, 'query')
+    while True:
+        driver.get(url)
 
-    select_query = f'SELECT \
-            repo_name, \
-            sum(event_type=\'ForkEvent\') AS forks, \
-            sum(event_type=\'WatchEvent\') AS stars, \
-            sum(event_type=\'IssuesEvent\') AS issues, \
-            sum(event_type=\'CommitCommentEvent\') AS commits, \
-            sum(event_type=\'PullRequestEvent\') AS pull_requests \
-            FROM github_events WHERE(event_type IN(\'ForkEvent\', \'WatchEvent\', \'IssuesEvent\', \'CommitCommentEvent\', \'PullRequestEvent\')) \
-            GROUP BY repo_name ORDER BY stars DESC, commits DESC LIMIT 2000 OFFSET {offset} \
-            '
-    query_input.send_keys(select_query)
+        query_input = driver.find_element(By.ID, 'query')
 
-    query_input.send_keys(Keys.CONTROL, Keys.RETURN)
-    time.sleep(10)
+        select_query = f'SELECT \
+                repo_name, \
+                sum(event_type=\'ForkEvent\') AS forks, \
+                sum(event_type=\'WatchEvent\') AS stars, \
+                sum(event_type=\'IssuesEvent\') AS issues, \
+                sum(event_type=\'CommitCommentEvent\') AS commits, \
+                sum(event_type=\'PullRequestEvent\') AS pull_requests \
+                FROM github_events WHERE(event_type IN(\'ForkEvent\', \'WatchEvent\', \'IssuesEvent\', \'CommitCommentEvent\', \'PullRequestEvent\')) \
+                GROUP BY repo_name ORDER BY stars DESC, commits DESC LIMIT 2500 OFFSET {offset} \
+                '
+        query_input.send_keys(select_query)
 
-    page_source = driver.page_source
+        query_input.send_keys(Keys.CONTROL, Keys.RETURN)
+        time.sleep(10)
 
-    soup = BeautifulSoup(page_source, 'html.parser')
+        page_source = driver.page_source
 
-    table = soup.find('table', {'id': 'data-table'})
+        soup = BeautifulSoup(page_source, 'html.parser')
 
-    for row in table.find_all('tr')[1:]:
-        columns = row.find_all('td')[1:]
-        row_data = [col.text.strip() for col in columns]
-        data.append(row_data)
+        table = soup.find('table', {'id': 'data-table'})
 
-    offset += 2_000
+        for row in table.find_all('tr'):
+            columns = row.find_all('td')[1:]
+            row_data = [col.text.strip() for col in columns]
+            data.append(row_data)
 
-driver.quit()
+        offset += 2_500
+
+        if offset < limit:
+            break
+
+    driver.quit()
+
+    return data
+
+
+data = parsingHTML(1_000_000)
 
 filename = 'data_github.csv'
-
 with open(filename, 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(data)

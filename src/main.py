2b3fa5accb5829d61f9560428b58ebed05a952d9
Goldria <dasha@igold.pp.ru>
2023-12-18 21:58:10 +0500

    Added gui (web) with random forest and gradient boosting.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,42 @@
+from flask import Flask, render_template, request
+
 from model_classifier import GitHubClassifier
 
+app = Flask(__name__, template_folder='templates')
 
-new_data = [[1, 1, 1, 1, 1, 1]]
-filename = '../training_data/data_github.csv'
 
-classifier = GitHubClassifier(filename)
-classifier.load_model('../trained_models/random_forest.pkl')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-print("Random Forest Classifier Metrics:")
-print(f'Precision: {classifier.count_precision()}')
-print(f'Recall: {classifier.count_recall()}')
-print(f'F1-score: {classifier.count_f1()}')
-class_rf = classifier.predict_class(new_data)
-print(
-    f'Predicted class using Random Forest Classifier: {class_rf}')
+
+@app.route('/result', methods=['POST'])
+def result():
+    data_from_post = []
+    repo = request.form['repo-name']
+    data_from_post.append(request.form['forks'])
+    data_from_post.append(request.form['stars'])
+    data_from_post.append(request.form['issues'])
+    data_from_post.append(request.form['commits'])
+    data_from_post.append(request.form['pull-requests'])
+    data_from_post.append(request.form['contributors'])
+
+    data = [int(x) if x.isdigit() else 0 for x in data_from_post]
+    model_data = [data]
+
+    filename = '../training_data/data_github.csv'
+    classifier = GitHubClassifier(filename)
+
+    model = request.form['model-choice']
+    if (model == 'gradient-boosting'):
+        classifier.load_model('../trained_models/gradien_boosting.pkl')
+        model = 'Градиентный бустинг'
+    if model == 'random-forest':
+        classifier.load_model('../trained_models/random_forest.pkl')
+        model = 'Случайный лес'
+    class_rf = classifier.predict_class(model_data)
+    return render_template('result.html', class_popularity=class_rf, model_data=data, repo_name=repo, model_name=model)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
